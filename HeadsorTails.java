589a5e2d8494cb5f03f854c2b9da9c3fc2a3a80a
細谷和真 <hosoyakazuma@hosoyawashinnoMacBook-Air.local>
2020-08-06 22:01:17 +0900

    User name added

--- HeadsorTails.java
+++ HeadsorTails.java
@@ -4,7 +4,15 @@ public class HeadsorTails{
     public static void main(String args[]){
         int i;
         int counts = 0;
+        String name;
+        
+        out.println("Who are you?");
+        System.out.print("> ");
+        name = new java.util.Scanner(System.in).nextLine();
+        System.out.println("Hello,"+name+"!");
+
         out.println("Tossing coin...");
+
         for(i=0;i<=2;i++){
             int rand = new java.util.Random().nextInt(2);
             out.print("Round"+(i+1)+":");
